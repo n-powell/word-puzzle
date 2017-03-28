44764be7641b3af29bbad52ed2f8616fef36f922
Jason Ainsworth and Nick Powell <nick@>
2017-03-28 13:32:37 -0700

    add extra if else to push letter outside of inner loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,43 @@
-var stringArray = [];
-var addItem = function(item) {
-  stringArray.push(item);
-};
 
 $(document).ready(function() {
 
-  $("#submit-button").click(function(event){
+  var newArray = [];
+  var vowels = ["a", "e", "i", "o", "u"];
+
+  $("#submit-button").click(function(event) {
     event.preventDefault();
-    var userInput = $("input").val().split("");
-    addItem(userInput);
-    alert(stringArray);
+    var stringArray = $("input").val().split("");
+    var charReplace = "";
+    // for(var index = 0; index <stringArray; index++){
+    //
+    // }
+    stringArray.forEach(function(letter) {
+      var foundVowell = false;
+      vowels.forEach(function(vowel) {      
+        if (letter === vowel) {
+          foundVowell = true;
+        }
+      });
+      if(foundVowell){
+        newArray.push("-");
+      }
+      else{
+        newArray.push(letter);
+      }
+
+    });
+    console.log(newArray);
   });
 
-
-
-
-
 });
+
+
+// for (i=0; i < 4; i++) {
+//   if (letter = vowel) {
+//     newArray.push("-");
+//     break;
+//   } else {
+//     newArray.push(letter);
+//     break;
+//   }
+// }
